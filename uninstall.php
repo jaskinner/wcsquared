6fdb3ddd332a6b6d72f3e288b063d6e3e0745c99
jaskinner <jon.a.skinner1@gmail.com>
2023-06-15 12:50:16 -0400

    drop db options when uninstalling

--- uninstall.php
+++ uninstall.php
@@ -11,3 +11,9 @@ global $wpdb;
 $table_name = $wpdb->prefix . 'wc_squared_locations';
 
 $wpdb->query("DROP TABLE IF EXISTS $table_name");
+
+$option_name_1 = 'wc_squared_api_key';
+$option_name_2 = 'wc_squared_sync_checkbox';
+
+delete_option($option_name_1);
+delete_option($option_name_2);
