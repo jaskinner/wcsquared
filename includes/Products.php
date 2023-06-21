339d545b9784e9ed46f143013ed7c94a136872a1
jaskinner <jon.a.skinner1@gmail.com>
2023-06-21 16:36:17 -0400

    a bit of error handling

--- includes/Products.php
+++ includes/Products.php
@@ -20,27 +20,31 @@ class Products
 
 	public function importProducts()
 	{
-		$api_key = get_option('wc_squared_api_key');
-		$client = new SquareClient([
-			'accessToken' => $api_key,
-			'environment' => Environment::SANDBOX,
-		]);
+		try {
+			$api_key = get_option('wc_squared_api_key');
+			$client = new SquareClient([
+				'accessToken' => $api_key,
+				'environment' => Environment::SANDBOX,
+			]);
 
-		$api_response = $client->getCatalogApi()->listCatalog(null, 'ITEM');
+			$api_response = $client->getCatalogApi()->listCatalog(null, 'ITEM');
 
-		if ($api_response->isSuccess()) {
-			$result = $api_response->getResult();
+			if ($api_response->isSuccess()) {
+				$result = $api_response->getResult();
 
-			foreach ($result->getObjects() as $object) {
-				if (count($object->getItemData()->getVariations()) <= 1) {
-					$this->createSimpleWooProduct($object->getItemData());
-				} else {
-					$this->createVariableWooProduct($object->getItemData());
+				foreach ($result->getObjects() as $object) {
+					if (count($object->getItemData()->getVariations()) <= 1) {
+						$this->createSimpleWooProduct($object->getItemData());
+					} else {
+						$this->createVariableWooProduct($object->getItemData());
+					}
+					// $this->insertOrUpdateProduct($object);
 				}
-				// $this->insertOrUpdateProduct($object);
+			} else {
+				$errors = $api_response->getErrors();
 			}
-		} else {
-			$errors = $api_response->getErrors();
+		} catch(\Exception $e) {
+			error_log('An error occurred during product import: ' . $e->getMessage());
 		}
 	}
 
@@ -62,65 +66,72 @@ class Products
 
 	private function createSimpleWooProduct($itemData)
 	{
-		$new_product = new WC_Product_Simple();
-		$variationData = $itemData->getVariations()[0]->getItemVariationData();
-		$new_product->set_sku($variationData->getSku());
-		$new_product->set_regular_price($variationData->getPriceMoney()->getAmount() / 100); // Assuming the price is in cents
+		try {
+			$new_product = new WC_Product_Simple();
+			$variationData = $itemData->getVariations()[0]->getItemVariationData();
+			$new_product->set_sku($variationData->getSku());
+			$new_product->set_regular_price($variationData->getPriceMoney()->getAmount() / 100); // Assuming the price is in cents
 
-		// Set product data
-		$new_product->set_name($itemData->getName());
-		$new_product->set_description($itemData->getDescriptionHtml());
-		$new_product->set_short_description($itemData->getDescriptionPlaintext());
+			// Set product data
+			$new_product->set_name($itemData->getName());
+			$new_product->set_description($itemData->getDescriptionHtml());
+			$new_product->set_short_description($itemData->getDescriptionPlaintext());
 
-		// Save the product
-		$product_id = $new_product->save();
+			// Save the product
+			$product_id = $new_product->save();
+		} catch(\Exception $e) {
+			error_log('An error occurred creating simple product: ' . $e->getMessage());
+		}
 	}
 
 	private function createVariableWooProduct($itemData)
 	{
+		try {
+			$variations = $itemData->getVariations();
 
-		$variations = $itemData->getVariations();
+			$new_product = new WC_Product_Variable();
 
-		$new_product = new WC_Product_Variable();
+			// Get all variation names
+			$variation_names = array();
+			foreach ($variations as $variation) {
+				$variationData = $variation->getItemVariationData();
+				$variation_names[] = $variationData->getName();
+			}
 
-		// Get all variation names
-		$variation_names = array();
-		foreach ($variations as $variation) {
-			$variationData = $variation->getItemVariationData();
-			$variation_names[] = $variationData->getName();
-		}
+			// one available for variation attribute
+			$attribute = new WC_Product_Attribute();
+			$attribute->set_name('Option');
+			$attribute->set_options($variation_names);
+			$attribute->set_position(0);
+			$attribute->set_visible(true);
+			$attribute->set_variation(true);
 
-		// one available for variation attribute
-		$attribute = new WC_Product_Attribute();
-		$attribute->set_name('Option');
-		$attribute->set_options($variation_names);
-		$attribute->set_position(0);
-		$attribute->set_visible(true);
-		$attribute->set_variation(true);
+			$new_product->set_attributes(array($attribute));
 
-		$new_product->set_attributes(array($attribute));
+			// Set product data
+			$new_product->set_name($itemData->getName());
+			$new_product->set_description($itemData->getDescriptionHtml());
+			$new_product->set_short_description($itemData->getDescriptionPlaintext());
 
-		// Set product data
-		$new_product->set_name($itemData->getName());
-		$new_product->set_description($itemData->getDescriptionHtml());
-		$new_product->set_short_description($itemData->getDescriptionPlaintext());
+			// Save the product
+			$product_id = $new_product->save();
 
-		// Save the product
-		$product_id = $new_product->save();
+			// If more than one variation exists, create them as separate products
+			foreach ($variations as $variation) {
+				$variationData = $variation->getItemVariationData();
 
-		// If more than one variation exists, create them as separate products
-		foreach ($variations as $variation) {
-			$variationData = $variation->getItemVariationData();
+				$new_variation = new WC_Product_Variation();
+				$new_variation->set_name($itemData->getName() . " - " . $variationData->getName());
+				$new_variation->set_parent_id($product_id);
+				$new_variation->set_regular_price($variationData->getPriceMoney()->getAmount() / 100); // Assuming the price is in cents
+				$new_variation->set_sku($variationData->getSku());
+				$new_variation->set_attributes( array( 'option' => $variationData->getName() ) );
 
-			$new_variation = new WC_Product_Variation();
-			$new_variation->set_name($itemData->getName() . " - " . $variationData->getName());
-			$new_variation->set_parent_id($product_id);
-			$new_variation->set_regular_price($variationData->getPriceMoney()->getAmount() / 100); // Assuming the price is in cents
-			$new_variation->set_sku($variationData->getSku());
-			$new_variation->set_attributes( array( 'option' => $variationData->getName() ) );
-
-			// Save the variation
-			$new_variation->save();
+				// Save the variation
+				$new_variation->save();
+			}
+		} catch(\Exception $e) {
+			error_log('An error occurred creating variable product: ' . $e->getMessage());
 		}
 	}
 }
