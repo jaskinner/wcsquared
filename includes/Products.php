8f04457ce38c18b6d7e47cdfcd163aaf5487f650
jaskinner <jon.a.skinner1@gmail.com>
2023-06-21 13:42:59 -0400

    variants are working

--- includes/Products.php
+++ includes/Products.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Products
  */
@@ -7,15 +8,18 @@ use Square\SquareClient;
 use Square\Environment;
 use Square\Exceptions\ApiException;
 
-class Products {
+class Products
+{
 	private $table_name_imported_products;
 
-	public function __construct() {
+	public function __construct()
+	{
 		global $wpdb;
 		$this->table_name_imported_products = $wpdb->prefix . 'wc_squared_imported_products';
 	}
 
-	public function importProducts() {
+	public function importProducts()
+	{
 		$api_key = get_option('wc_squared_api_key');
 		$client = new SquareClient([
 			'accessToken' => $api_key,
@@ -40,7 +44,8 @@ class Products {
 		}
 	}
 
-	private function insertOrUpdateProduct($product) {
+	private function insertOrUpdateProduct($product)
+	{
 		global $wpdb;
 
 		$productId = $product->getId();
@@ -55,7 +60,8 @@ class Products {
 		);
 	}
 
-	private function createSimpleWooProduct($itemData) {
+	private function createSimpleWooProduct($itemData)
+	{
 		$new_product = new WC_Product_Simple();
 		$variationData = $itemData->getVariations()[0]->getItemVariationData();
 		$new_product->set_sku($variationData->getSku());
@@ -70,32 +76,40 @@ class Products {
 		$product_id = $new_product->save();
 	}
 
-	private function createVariableWooProduct($itemData) {
-	
+	private function createVariableWooProduct($itemData)
+	{
+
 		$variations = $itemData->getVariations();
-	
+
 		$new_product = new WC_Product_Variable();
+
+		// Get all variation names
+		$variation_names = array();
+		foreach ($variations as $variation) {
+			$variationData = $variation->getItemVariationData();
+			$variation_names[] = $variationData->getName();
+		}
 
 		// one available for variation attribute
 		$attribute = new WC_Product_Attribute();
-		$attribute->set_name( 'Magical' );
-		$attribute->set_options( array( 'Yes', 'No' ) );
-		$attribute->set_position( 0 );
-		$attribute->set_visible( true );
-		$attribute->set_variation( true ); // here it is
-			
-		$new_product->set_attributes( array( $attribute ) );
-	
+		$attribute->set_name('Option');
+		$attribute->set_options($variation_names);
+		$attribute->set_position(0);
+		$attribute->set_visible(true);
+		$attribute->set_variation(true);
+
+		$new_product->set_attributes(array($attribute));
+
 		// Set product data
 		$new_product->set_name($itemData->getName());
 		$new_product->set_description($itemData->getDescriptionHtml());
 		$new_product->set_short_description($itemData->getDescriptionPlaintext());
-	
+
 		// Save the product
 		$product_id = $new_product->save();
-	
+
 		// If more than one variation exists, create them as separate products
-		foreach($variations as $variation) {
+		foreach ($variations as $variation) {
 			$variationData = $variation->getItemVariationData();
 
 			$new_variation = new WC_Product_Variation();
@@ -103,6 +117,7 @@ class Products {
 			$new_variation->set_parent_id($product_id);
 			$new_variation->set_regular_price($variationData->getPriceMoney()->getAmount() / 100); // Assuming the price is in cents
 			$new_variation->set_sku($variationData->getSku());
+			$new_variation->set_attributes( array( 'option' => $variationData->getName() ) );
 
 			// Save the variation
 			$new_variation->save();
