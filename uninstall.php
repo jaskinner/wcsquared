806b919e70a60fb0b67e73c3fef3e9ed15151ee5
Jon Skinner <jon.a.skinner1@gmail.com>
2023-06-29 13:38:07 -0400

    Update uninstall.php
    fixed some errors on cleanup

--- uninstall.php
+++ uninstall.php
@@ -8,13 +8,13 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-$table_name = $wpdb->prefix . 'wc_squared_locations';
+$inventory_table = $wpdb->prefix . 'wc_squared_inventory';
+$locations_table = $wpdb->prefix . 'wc_squared_locations';
 
 $wpdb->query("DROP TABLE IF EXISTS $table_name");
 
 $option_name_1 = 'wc_squared_api_key';
 $option_name_2 = 'wc_squared_sync_checkbox';
-$option_name_2 = 'wc_squared_sb_checkbox';
 
 delete_option($option_name_1);
 delete_option($option_name_2);
